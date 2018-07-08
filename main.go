2ecaab388ef4b82bab50f1531ce6dd2ebe145763
Geoff d'Alelio <gdalelio@users.noreply.github.com>
2018-07-08 18:32:50 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"learn-golang/lessons"
+)
+
+func main() {
+	// fmt.Println("test")
+	// lessons.Forloop()
+	// result := lessons.Sqrt2(9)
+	// fmt.Println("the result of Sqrt2(9) is: ", result)
+	// f1 := lessons.Pow(3, 2, 10) //returns 3 squared if result is less than 10 else 10 if higher
+	// fmt.Printf("return value from the Pow function is: %f\n", f1)
+	// f2 := lessons.Pow(3, 3, 20) //returns 3 cubed if result is less than 20 else 20 if higher
+	// fmt.Printf("return value from the Pow function is: %f\n", f2)
+	// fmt.Printf("%f %f \n", lessons.Pow(3, 2, 10), lessons.Pow(3, 3, 20))
+	//
+	// x := float64(2)
+	// fmt.Printf("Calculating the square root of %f is %f\n", x, exercises.Sqrt(x))
+	// x = float64(3)
+	// fmt.Printf("Calculating the square root of %f is %f\n", x, exercises.Sqrt(x))
+	// x = float64(4)
+	// fmt.Printf("Calculating the square root of %f is %f\n", x, exercises.Sqrt(x))
+	//
+	// lessons.GoOS()
+	// lessons.Deferer()
+	//
+	// lessons.Pointers()
+	// lessons.Struc()
+	// lessons.Channels()
+	lessons.BufferedConcurrency()
+}
