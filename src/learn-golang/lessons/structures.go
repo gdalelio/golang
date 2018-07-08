9cb9e6a12fd5e1e606e4003bd82c46f3d81fc5d0
Geoff d'Alelio <gdalelio@users.noreply.github.com>
2018-07-08 18:29:22 -0400

    adding files from laptop
    files from laptop lessons

--- src/learn-golang/lessons/structures.go
+++ src/learn-golang/lessons/structures.go
@@ -6,18 +6,29 @@ import "fmt"
 type Vertex struct {
 	x int
 	y int
-	z int
 }
 
 //Struc uses vertex structure
-func Struc(x int, y int, z int) {
-	point := Vertex{x, y, z}
+func Struc() {
+	point := Vertex{}
 	fmt.Println(point)
 	fmt.Println("The x value is: ", point.x)
 	fmt.Println("The y value is: ", point.y)
-	fmt.Println("The z value is: ", point.z)
-	fmt.Printf("The vertex is located at: (%d, %d, %d)\n", point.x, point.y, point.z)
-	p := &point //returns a pointer to point variable
-	p.x = 1e9   //replace the value of x with 1e9
-	fmt.Println(p.x)
+	fmt.Printf("The vertex is located at: (%d, %d)\n", point.x, point.y)
+	fmt.Println("printing vertexes v1,p, v2, and v3")
+	fmt.Println(v1, p, v2, v3)
+	fmt.Printf("p.x = %d\n", p.x)
+	fmt.Printf("p = %d\n", p)
+	p2.x = 4
+	fmt.Printf("p2.x = %d\n", p2.x)
+	fmt.Printf("p = %d\n", p)
+
 }
+
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{x: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+	p2 = p
+)
